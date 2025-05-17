3cced459a5ec7e375f7289188e9433e9567e8bf7
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-05-18 03:12:23 +0700

    kriteria 3

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -66,6 +66,8 @@ with mlflow.start_run(run_name="Random Forest with Tuning") as run:
     print("✅ Model logged to MLflow.")
 
     artifact_path = os.path.join(os.getcwd(), "mlflow")
+    os.makedirs(artifact_path, exist_ok=True)
+    
     artifact_uri=f"./mlruns/{experiment_id}/{run_id}/artifacts"
     mlflow.artifacts.download_artifacts(artifact_uri=artifact_uri, dst_path=artifact_path)
     print("✅ Model artifact downloaded.")
