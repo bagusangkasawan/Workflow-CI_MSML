e96ae775deeb0c6a9ac46a3d004c1bfed666d9dc
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-05-23 13:30:21 +0700

    kriteria 3

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,15 +3,12 @@ import mlflow
 import mlflow.sklearn
 from mlflow.models.signature import infer_signature
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, confusion_matrix, f1_score
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 import pandas as pd
 
 mlflow.set_experiment("Obesity_Classification")
 mlflow.sklearn.autolog(log_models=False)
-
-experiment = mlflow.get_experiment_by_name("Obesity_Classification")
-experiment_id = experiment.experiment_id
 
 BASE_DIR = os.path.dirname(__file__)
 df = pd.read_csv(os.path.join(BASE_DIR, "obesity_data_preprocessing.csv"))
@@ -30,43 +27,44 @@ rf_param_grid = {
 rf_model = RandomForestClassifier(random_state=42)
 grid_search = GridSearchCV(estimator=rf_model, param_grid=rf_param_grid, cv=3, n_jobs=-1, verbose=2)
 
-run = mlflow.active_run()
-run_id = run.info.run_id if run else None
+with mlflow.start_run(nested=True, run_name="Random Forest with Tuning") as run:
+    run_id = run.info.run_id
+    experiment_id = run.info.experiment_id
 
-grid_search.fit(X_train, y_train)
-best_rf_model = grid_search.best_estimator_
+    grid_search.fit(X_train, y_train)
+    best_rf_model = grid_search.best_estimator_
 
-y_pred = best_rf_model.predict(X_test)
+    y_pred = best_rf_model.predict(X_test)
 
-report_dict = classification_report(y_test, y_pred, output_dict=True)
-f1_macro = report_dict["macro avg"]["f1-score"]
-class_0_f1 = report_dict.get("0", {}).get("f1-score", 0)
-num_classes = len(report_dict) - 3
-cm = confusion_matrix(y_test, y_pred)
-tn = cm[0][0]
-fp = cm[0][1]
-specificity = tn / (tn + fp)
-f1_class_1 = f1_score(y_test, y_pred, average='macro')
+    report_dict = classification_report(y_test, y_pred, output_dict=True)
+    f1_macro = report_dict["macro avg"]["f1-score"]
+    class_0_f1 = report_dict.get("0", {}).get("f1-score", 0)
+    num_classes = len(report_dict) - 3
+    cm = confusion_matrix(y_test, y_pred)
+    tn = cm[0][0]
+    fp = cm[0][1]
+    specificity = tn / (tn + fp)
+    f1_class_1 = f1_score(y_test, y_pred, average='macro')
 
-mlflow.log_param("model_type", "Random Forest")
-mlflow.log_param("num_classes", num_classes)
-mlflow.log_metric("f1_macro", f1_macro)
-mlflow.log_metric("f1_class_0", class_0_f1)
-mlflow.log_metric("specificity", specificity)
-mlflow.log_metric("f1_class_1", f1_class_1)
+    mlflow.log_param("model_type", "Random Forest")
+    mlflow.log_param("num_classes", num_classes)
+    mlflow.log_metric("f1_macro", f1_macro)
+    mlflow.log_metric("f1_class_0", class_0_f1)
+    mlflow.log_metric("specificity", specificity)
+    mlflow.log_metric("f1_class_1", f1_class_1)
 
-input_example = X_train.iloc[:1].astype("float64")
-signature = infer_signature(X_train.astype("float64"), best_rf_model.predict(X_train))
-    
-mlflow.sklearn.log_model(
-    sk_model=best_rf_model,
-    artifact_path="random_forest_model",
-    input_example=input_example,
-    signature=signature
-)
-print("✅ Model logged to MLflow.")
+    input_example = X_train.iloc[:1].astype("float64")
+    signature = infer_signature(X_train.astype("float64"), best_rf_model.predict(X_train))
 
-artifact_path = os.path.join(os.getcwd(), "artifacts")
-artifact_uri=f"./mlruns/{experiment_id}/{run_id}/artifacts/"
-mlflow.artifacts.download_artifacts(artifact_uri=artifact_uri, dst_path=artifact_path)
-print("✅ Model artifact downloaded.")
+    mlflow.sklearn.log_model(
+        sk_model=best_rf_model,
+        artifact_path="random_forest_model",
+        input_example=input_example,
+        signature=signature
+    )
+    print("✅ Model logged to MLflow.")
+
+    artifact_path = os.path.join(os.getcwd(), "artifacts")
+    artifact_uri = f"runs:/{run_id}/random_forest_model"
+    mlflow.artifacts.download_artifacts(artifact_uri=artifact_uri, dst_path=artifact_path)
+    print("✅ Model artifact downloaded.")
