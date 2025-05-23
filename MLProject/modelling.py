8858e7ab3827c03bdcf141a55c3bc7d9b6688c77
Bagus Angkasawan Sumantri Putra <bagusasp02@gmail.com>
2025-05-23 14:18:48 +0700

    kriteria 3

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -64,6 +64,6 @@ mlflow.sklearn.log_model(
 print("✅ Model logged to MLflow.")
 
 artifact_path = os.path.join(os.getcwd(), "artifacts")
-artifact_uri=f"./mlruns/{experiment_id}/None/artifacts/"
+artifact_uri=f"./artifacts/"
 mlflow.artifacts.download_artifacts(artifact_uri=artifact_uri, dst_path=artifact_path)
 print("✅ Model artifact downloaded.")
